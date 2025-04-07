9cf2a06fc3256030820f68cb38985f3296ce40a9
EdgarSR-1 <A01286059@tec.mx>
2025-04-07 16:37:56 -0600

    tarea 2 inicio

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hola Mundo!");
+
+
+
+const saludo = () => {
+    console.log("Hola");
+}
+
+saludo();
+
+function msg() {
+    console.log("TEST");
+}
+
+function msg(name) {
+    console.log("Hola " + name);
+}
+
+msg("Luis");
